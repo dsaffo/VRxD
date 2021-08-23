740814dc9c7d54ebbee431f11dba7207ec9fcccf
david saffo <saffo.d@husky.neu.edu>
2021-08-23 00:34:11 -0400

    new chartpoint compenent for more efficient charts in vr

---
+++ baseball/src/VR/chartpoint.js
@@ -0,0 +1,48 @@
+AFRAME.registerComponent('chartpoint', {
+    schema: {
+        radius: {type: 'number', default: 1},
+        segments: {type: 'number', default: 8},
+        color: { default: '#000'},
+        opacity: {type: 'number', default: 1}
+    },
+    
+    init: function () {
+        var data = this.data;
+        var el = this.el;
+       
+        const geometry = new THREE.CircleGeometry( data.radius, data.segments );
+        const material = new THREE.MeshBasicMaterial( { color: new THREE.Color(data.color), opacity: data.opacity, transparent: true});
+        const circle = new THREE.Mesh( geometry, material );
+
+        el.setObject3D('mesh', circle);
+    },
+
+    update: function (oldData) {
+        var data = this.data;
+        var el = this.el;
+    
+        // If `oldData` is empty, then this means we're in the initialization process.
+        // No need to update.
+        if (Object.keys(oldData).length === 0) { return; }
+    
+        // Geometry-related properties changed. Update the geometry.
+        if (data.radius !== oldData.radius) {
+          el.getObject3D('mesh').geometry = new THREE.CircleGeometry( data.radius, data.segments );
+        }
+    
+        // Material-related properties changed. Update the material.
+        if (data.color !== oldData.color) {
+          el.getObject3D('mesh').material.color = new THREE.Color(data.color);
+        }
+
+        if (data.opacity !== oldData.opacity) {
+            el.getObject3D('mesh').material.opacity = data.opacity;
+        }
+    },
+
+    remove: function () {
+        this.el.removeObject3D('mesh');
+    },
+  
+    multiple: true
+});
